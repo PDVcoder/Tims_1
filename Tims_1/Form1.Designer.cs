cb4f88bad03084bc60997a28f972c7cc517cbd8d
DMYTRO PAIEVSKYI <payevskyidima@gmail.com>
2021-02-05 17:50:10 +0200

    Tims_1 v1

---
+++ Tims_1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Tims_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_count = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(12, 12);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(100, 23);
+            this.button_clear.TabIndex = 0;
+            this.button_clear.Text = "Очистити";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Далі";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Виконати n ітерацій";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(118, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox_count
+            // 
+            this.textBox_count.Location = new System.Drawing.Point(43, 115);
+            this.textBox_count.Name = "textBox_count";
+            this.textBox_count.Size = new System.Drawing.Size(69, 20);
+            this.textBox_count.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "n = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 39);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Вірогідність \r\nпотрапляння \r\nв коло:";
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(12, 180);
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(100, 20);
+            this.textBox_result.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 421);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_count);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_clear);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(540, 460);
+            this.MinimumSize = new System.Drawing.Size(540, 460);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tims 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_result;
+    }
+}
+
---
+++ Tims_1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tims_1
+{
+    public partial class Form1 : Form
+    {
+        Pen pen1, pen2;
+        Bitmap bmp;
+        Graphics gp;
+        Color col;
+        Random rand;
+        int x = 0, y = 0;
+        double r = 0;
+        int n = 0;
+        double k = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitStartSpace();
+            col = Color.Red;
+            rand = new Random(0);
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            InitStartSpace();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = rand.Next(1, pictureBox1.Width - 1);
+            y = rand.Next(1, pictureBox1.Height - 1);
+            r = Math.Sqrt(Convert.ToDouble(Math.Pow(x - pictureBox1.Width / 2, 2) + Math.Pow(y - pictureBox1.Height / 2, 2)));
+            if (r > pictureBox1.Width / 2)
+            {
+                col = Color.Green;
+            }
+            else
+            {
+                col = Color.Red;
+                k++;
+            }
+            n++;
+            bmp.SetPixel(x, y, col);
+            bmp.SetPixel(x + 1, y, col);
+            bmp.SetPixel(x, y + 1, col);
+            bmp.SetPixel(x + 1, y + 1, col);
+            pictureBox1.Image = bmp;
+            textBox_result.Text = Convert.ToString(k / n);
+            textBox_count.Text = Convert.ToString(n);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            n = 0;
+            k = 0;
+            try
+            {
+                n = Convert.ToInt32(textBox_count.Text);
+            }
+            catch (Exception)
+            {
+                textBox_count.Text = "Помилка";
+            }
+            InitStartSpace();
+            
+            for (int i = 0; i < n; i++)
+            {
+                x = rand.Next(1, pictureBox1.Width - 1);
+                y = rand.Next(1, pictureBox1.Height - 1);
+                r = Math.Sqrt(Convert.ToDouble(Math.Pow(x - pictureBox1.Width / 2, 2) + Math.Pow(y - pictureBox1.Height / 2, 2)));
+                if (r > pictureBox1.Width / 2)
+                {
+                    col = Color.Green;
+                }
+                else
+                {
+                    col = Color.Red;
+                    k++;
+                }
+                bmp.SetPixel(x, y, col);
+                bmp.SetPixel(x + 1, y, col);
+                bmp.SetPixel(x, y + 1, col);
+                bmp.SetPixel(x + 1, y + 1, col);
+                pictureBox1.Image = bmp;
+            }
+            textBox_result.Text = Convert.ToString(k / n);
+        }
+
+
+        private void InitStartSpace()
+        {
+            pen1 = new Pen(Color.Black, 4);
+            pen2 = new Pen(Color.Black, 2);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gp = Graphics.FromImage(bmp);
+            gp.DrawRectangle(pen1, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            gp.DrawEllipse(pen2, 2, 2, pictureBox1.Width - 2, pictureBox1.Height - 2);
+            pictureBox1.Image = bmp;
+            n = 0; k = 0;
+        }
+    }
+}
